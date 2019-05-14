b7f7c1158112febf7352ebaf483ca7ab88a93acc
gm9789823775 <muthu.gokul@gmail.com>
2019-05-14 13:04:56 -0700

    Created the model for task and parent task

---
+++ TaskManagerApi.Model/ParentTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TaskManagerApi.Model
+{
+    /// <summary>
+    /// Parent Task model.
+    /// </summary>
+    public class ParentTask
+    {
+        /// <summary>
+        /// Unique id of parent task.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Name of parent task.
+        /// </summary>
+        public string Name{ get; set; }
+    }
+}
---
+++ TaskManagerApi.Model/Task.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TaskManagerApi.Model
+{
+    /// <summary>
+    /// Task model.
+    /// </summary>
+    public class Task
+    {
+        /// <summary>
+        /// Unique id of task.
+        /// </summary>
+        public Guid Id{ get; set; }
+
+        /// <summary>
+        /// Name of the task.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Priority of task.
+        /// </summary>
+        public int priority { get; set; }
+
+        /// <summary>
+        /// Parent task of task.
+        /// </summary>
+        public ParentTask ParentTask { get; set; }
+
+        /// <summary>
+        /// Start date of the task.
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// End date of the task.
+        /// </summary>
+        public DateTime? EndDate { get; set; }
+    }
+}
